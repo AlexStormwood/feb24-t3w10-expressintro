1153458b23e835bc218e86e5fb21fbccea390aa0
Alex <alex.holder@bigfootds.com>
2024-10-15 20:01:38 +1100

    feat: server hello world implemented

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+// Make an instance of the Express system
+// so we can configure it 
+// eg. routes, settings
+const app = express();
+
+// GET localhost:3000/ 
+// .get("/", (req, res) => {})
+// .get("/", (banana, elephant) => {})
+app.get("/", (request, response) => {
+	// response.send("<h1>Hello world!</h1>");
+	response.json({
+		message:"Hello world!"
+	});
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+	console.log(`Server listening on localhost:${PORT}`);
+});
